f886bef0b0b2a0708d9ca3d6df666f6047c2dcc3
aaarunan <arunan2212@gmail.com>
2023-02-09 15:09:35 +0100

    Add HTML GET on connection

--- src/ClientConnection.java
+++ src/ClientConnection.java
@@ -17,38 +17,22 @@ public class ClientConnection implements Runnable {
 
     @Override
     public void run() {
-        while (true) {
-            String response = null;
-            try {
-                response = in.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            String[] split = response.split("-");
-
-            if (split.length > 2) {
-                throw new IllegalArgumentException("To many operator signs, only one allowed.");
-            }
-            if (split.length < 2) {
-                throw new IllegalArgumentException("Wrong format, too few arguments");
-            }
-
-            int sum;
-
-            try {
-                int num1 = Integer.parseInt(split[0]);
-                int num2 = Integer.parseInt(split[1]);
-                sum = num1 - num2;
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Input must be integers.");
-            }
-            out.println(sum);
+        out.println("HTTP/1.1 200 OK");
+        out.println("Content-Type: text/html");
+        out.println("\r\n");
+        out.println("<p> Hello world </p>");
+        out.flush();
+        try {
+            this.stop();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void stop() throws IOException {
-        in.close();
+        out.flush();
         out.close();
+        in.close();
         socket.close();
     }
 
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
-        int port = 6666;
+        int port = 8000;
 
         try {
             server.start(port);
